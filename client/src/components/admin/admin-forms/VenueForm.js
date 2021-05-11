bc155cdb59311cb56f0b419fd7991416969dccfc
Felipe Falco <falco.felipe27@gmail.com>
2021-05-11 12:18:11 -0700

    Fixed an error with updating the Venues description\

--- client/src/components/admin/admin-forms/VenueForm.js
+++ client/src/components/admin/admin-forms/VenueForm.js
@@ -2,61 +2,63 @@ import React, { useState, useEffect } from 'react';
 import Form from 'react-bootstrap/Form';
 import { useHome, updateVenue } from '../../../context/home/HomeState';
 
-const VenueForm = props => {
-  const homeDispatch = useHome()[1];
+const VenueForm = (props) => {
+	const homeDispatch = useHome()[1];
 
-  const initialState = props.state.venue;
-  const { action } = props.state;
+	const initialState = props.state.venue;
+	const { action } = props.state;
 
-  const formId = `venue-form-${action}`;
+	const formId = `venue-form-${action}`;
 
-  const [venue, setVenue] = useState(initialState);
+	const [venue, setVenue] = useState(initialState);
 
-  useEffect(() => {
-    setVenue(props.state.venue);
-  }, [props.state.venue]);
+	useEffect(() => {
+		setVenue(initialState);
+	}, [initialState]);
 
-  let { location, description } = venue;
+	let { location, description } = venue;
 
-  const onChange = e =>
-    setVenue({
-      ...venue,
-      [e.target.name]: e.target.value,
-    });
+	const onChange = (e) =>
+		setVenue({
+			...venue,
+			[e.target.name]: e.target.value,
+		});
 
-  const onSubmit = e => {
-    e.preventDefault();
+	const onSubmit = (e) => {
+		e.preventDefault();
 
-    updateVenue(homeDispatch, venue);
-    if (props.history) {
-      props.history.goBack();
-    }
+		updateVenue(homeDispatch, venue);
+		if (props.history) {
+			props.history.goBack();
+		}
 
-    return false;
-  };
+		return false;
+	};
 
-  return (
-    <Form id={formId} onSubmit={onSubmit}>
-      <Form.Group>
-        <Form.Label htmlFor='location'>Location</Form.Label>
-        <Form.Control
-          type='text'
-          name='location'
-          value={location}
-          onChange={onChange}
-        />
-      </Form.Group>
-      <Form.Group>
-        <Form.Label htmlFor='venue-description'>Venue Description</Form.Label>
-        <Form.Control
-          as='textarea'
-          name='venue-description'
-          value={description}
-          onChange={onChange}
-          rows={4}></Form.Control>
-      </Form.Group>
-    </Form>
-  );
+	return (
+		<Form id={formId} onSubmit={onSubmit}>
+			<Form.Group>
+				<Form.Label htmlFor='location'>Location</Form.Label>
+				<Form.Control
+					type='text'
+					name='location'
+					value={location}
+					onChange={onChange}
+				/>
+			</Form.Group>
+			<Form.Group>
+				<Form.Label htmlFor='venue-description'>
+					Venue Description
+				</Form.Label>
+				<Form.Control
+					as='textarea'
+					name='description'
+					value={description}
+					onChange={onChange}
+					rows={4}></Form.Control>
+			</Form.Group>
+		</Form>
+	);
 };
 
 export default VenueForm;
